5b6f5784acc1409ff218777ee5f6f06582459a76
Anyafulugo <comkemmachi@gmail.com>
2023-06-14 05:05:56 -0700

    static libraries

---
+++ 0x09-static_libraries/0-memset.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _memset - fill a block of memory
+ * @s: starting address
+ * @b: desired value
+ * @n: number of bytes to be changed
+ *
+ * Return: changed array
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+}
