42a3668736267c2b5f57c8174e33f583c9cbb88d
Anyafulugo <comkemmachi@gmail.com>
2023-06-14 06:36:58 -0700

    static libraries

---
+++ 0x09-static_libraries/1-strncat.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * _strncat - concatenate two strings
+ * @dest: input values
+ * @src: input value
+ * @n: input value
+ *
+ * Return: dest
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+}
---
+++ 0x09-static_libraries/2-strchr.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * _strchr - entry point
+ * @s: input
+ * @c: input
+ * Return: Always 0
+ */
+char *_strchr(char *s, char c)
+{
+}
